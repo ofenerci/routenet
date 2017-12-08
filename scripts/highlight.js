bd3a821aeab1bf09abfdacad07c7be5e19a1dc82
Arne de Laat <arne@delaat.net>
2017-12-08 19:04:17 +0100

    Refactor HTML, CSS, and JS. Upgrade pages to html5. Remove 'nietGekozen' class. Use 'data-next' instead of 'title' to hold references to related items. Use jQuery selectors as references for simpler scripts. Highlight text in white on hover. Reorder classes in CSS giving more priority to classes closer to source. Refactor Javascript to use jQuery more effectively.

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,95 +1,45 @@
 // Auteur: N.G. Schultheiss
-// This script controls the css classes: '.vervolg#' and 'nietGkozen'.
+// This script controls the css classes: '.vervolg#' to highlight cells.
 
-var klasA = new Array();
-var klasB = new Array();
-var klasC = new Array();
-var klasD = new Array();
-
+// Add hover event handlers to all non-empty cells
 $(document).ready(function () {
-    $('#routenet td').not('.empty').hover(function() {select(this.id);},
-                                          function() {deselect(this.id);});
+    $('#routenet td').not('.empty').hover(
+        function() {select($(this));},
+        function() {deselect();}
+    );
 });
 
-
-// FIXME: Make recursive
-function maakLijst(id) {
-    var elementA = $('#' + id);
-    var colA = elementA.attr("title");
-    if(colA !==  '') {
-        var attr = '';
-        for(i=0; i <= colA.length; i++) {
-            if((colA.substr(i, 1) == ',') || (i == colA.length)) {
-                klasA.push(attr);
-                var elementB = $('#' + attr);
-                attr = '';
-                var colB = elementB.attr("title");
-                if(colB !==  '') {
-                    attr = '';
-                    for (j=0; j <= colB.length; j++) {
-                        if((colB.substr(j, 1) == ',') || (j == colB.length)) {
-                            klasB.push(attr);
-                            var elementC = $('#' + attr);
-                            attr = '';
-                            var colC = elementC.attr("title");
-                            if(colC !==  '') {
-                                attr = '';
-                                for (k=0; k <= colC.length; k++) {
-                                    if((colC.substr(k, 1) == ',') || (k == colC.length)) {
-                                        klasC.push(attr);
-                                        var elementD = $('#' + attr);
-                                        attr = '';
-                                        var colD = elementD.attr("title");
-                                        if(colD !==  '') {
-                                            attr = '';
-                                            for (l=0; l <= colD.length; l++) {
-                                                if((colD.substr(l, 1) == ',') || (l == colD.length)) {
-                                                    klasD.push(attr);
-                                                    attr = '';}
-                                                else attr += colD.substr(l, 1);}}
-                                        attr = '';}
-                                    else attr += colC.substr(k, 1);}}
-                            attr = '';}
-                        else attr += colB.substr(j, 1);}}}
-            else attr += colA.substr(i, 1);}}
+function getRelated(source) {
+    return $(source.data("next"));
 }
 
+function select(source) {
+    var level1 = getRelated(source);
+    var level2 = $();
+    var level3 = $();
+    var level4 = $();
 
-// Het verwerken van de informatie.
-function highlightTree(id) {
-    var attr;
-    var element;
+    level1.each(function() {
+        level2 = level2.add(getRelated($(this)));
+    })
 
-    for(k=0; klasD.length > 0;) {
-        attr = klasD.pop()
-        element = $('#' + attr);
-        element.attr('class', 'vervolg4');}
-    for(k=0; klasC.length > 0;) {
-        attr = klasC.pop()
-        element = $('#' + attr);
-        element.attr('class', 'vervolg3');}
-    for(k=0; klasB.length > 0;) {
-        attr = klasB.pop()
-        element = $('#' + attr);
-        element.attr('class', 'vervolg2');}
-    for(k=0; klasA.length > 0;) {
-        attr = klasA.pop()
-        element = $('#' + attr);
-        element.attr('class', 'vervolg1');}
-    element = $('#' + id);
-    element.attr('class', 'vervolg0');
-}
+    level2.each(function() {
+        level3 = level3.add(getRelated($(this)));
+    })
 
+    level3.each(function() {
+        level4 = level4.add(getRelated($(this)));
+    })
 
-// Met deze functie zijn de klassen te kiezen.
-function select(id) {
-    maakLijst(id);
-    highlightTree(id);
+    source.addClass('vervolg0');
+    level1.addClass('vervolg1');
+    level2.addClass('vervolg2');
+    level3.addClass('vervolg3');
+    level4.addClass('vervolg4');
 }
 
 
 // Met deze functie zijn de klassen weer op 'nietGekozen' te zetten.
-function deselect(id) {
-    for (var i=0; i < 5; i++) {
-        $('td.vervolg' + i).attr('class','nietGekozen');}
+function deselect() {
+    $('#routenet td').removeClass('vervolg0 vervolg1 vervolg2 vervolg3 vervolg4');
 }
